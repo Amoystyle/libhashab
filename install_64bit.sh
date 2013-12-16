2b6e8d35aeba3a55bd87f6c1a57bf71491029caf
Deny Dias <deny@macpress.com.br>
2013-12-16 11:58:48 -0200

    Fix libhashab32.so path. Add chmod 777 library paths (commented out).

--- install_64bit.sh
+++ install_64bit.sh
@@ -5,7 +5,7 @@ mkdir -p /usr/lib64/libgpod/
 cp libhashab32.so /usr/lib/x86_64-linux-gnu/libgpod/libhashab32.so
 cp libhashab32_wrapper /usr/lib/x86_64-linux-gnu/libgpod/libhashab32_wrapper
 cp libhashab64.so /usr/lib/x86_64-linux-gnu/libgpod/libhashab.so
-cp libhashab32.so /usr/lib64libgpod/libhashab32.so
+cp libhashab32.so /usr/lib64/libgpod/libhashab32.so
 cp libhashab32_wrapper /usr/lib64/libgpod/libhashab32_wrapper
 cp libhashab64.so /usr/lib64/libgpod/libhashab.so
-
+#chmod -R 777 /usr/lib/x86_64-linux-gnu/libgpod/ /usr/lib64/libgpod/
